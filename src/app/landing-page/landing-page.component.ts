ed3ca25fb1c9cd9d115f2037b030539dffdd9744
Bibeat34 <roussy.nathan@gmail.com>
2025-01-30 15:29:25 +0100

    Ajout d'un boutton, Ajout de descriptions aux bouttons

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -11,5 +11,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  hoveredButton: string | null = null;
+  
+  showHoverText(button: string) {
+    this.hoveredButton = button;
+  }
 
+  clearHoverText() {
+    this.hoveredButton = null;
+  }
+
+  getHoverText(button: string): string {
+    switch(button) {
+      case 'callback':
+        return "L'équipe de support informatique/métier vous recontacte dès que possible";
+      case 'create':
+        return "Vous pouvez créer un ticket pour signaler un incident, proposer une amélioration, ...";
+      case 'list':
+        return "Vous pouvez consulter vos tickets pour en suivre l'avancée";
+      default:
+        return '';
+    }
+  }
 }
